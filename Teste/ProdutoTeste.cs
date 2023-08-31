51083bd5b0931b34ef042fd44d2680f0ebea44a0
Ícaro <icarobotelhosocial@gmail.com>
2023-08-30 22:14:23 -0300

    Introdução a testes segundo dia.

--- Teste/ProdutoTeste.cs
+++ Teste/ProdutoTeste.cs
@@ -5,7 +5,7 @@ namespace Teste
    consumidos pelos funcionarios da empresa
 
    Critérios de aceite:
-   - Um novo produto deve ter obrigatoriamente um codigo único, nome, saldo 0, custo 0, unidade de medida.
+   - Check: Um novo produto deve ter obrigatoriamente um codigo único, nome, saldo 0, custo 0, unidade de medida.
    - As unidades de medidas são: PC, KG, MT, GR, LT, CX.
    - Um produto PODE ter uma descrição.
    - Um produto pode ter um codigo de barras.
@@ -14,9 +14,56 @@ namespace Teste
 {
     public class ProdutoTeste
     {
-        [Fact]
-        public void Test1()
+        //Propriedades.
+
+        private int _codigo;
+        private string _nome, _medida;
+        private float _saldo, _custo;
+
+        //Metodo construtor.
+        
+        public ProdutoTeste()
         {
+            _codigo = 1;
+            _nome = "Mouse";
+            _medida = "PC";
+            _saldo = 0;
+            _custo = 0;
+        }
+
+        public class Produto
+        {
+            public int Codigo { get; }
+            public string Nome { get; }
+            public string Medida { get; }
+            public float Saldo { get; }
+            public float Custo { get; }
+            public Produto(int codigo, string nome, string medida, float saldo, float custo)
+            {
+                Codigo = codigo;
+                Nome = nome;
+                Medida = medida;
+                Saldo = saldo;
+                Custo = custo;
+            }
+        }
+
+        [Fact] // Theory: Repede e pede o teste com parametros, diferente de Fact que roda apenas uma vez
+        public void CriarObjeto()
+        {
+            //Arrange: crio as variaveis.
+
+            //Act: criamos o objeto.
+
+            Produto prod = new Produto(_codigo, _nome, _medida, _saldo, _custo);
+
+            //Assert: verifico se ele foi criado.
+
+            Assert.Equal(_codigo, prod.Codigo);
+            Assert.Equal(_nome, prod.Nome);
+            Assert.Equal(_medida, prod.Medida);
+            Assert.Equal(_saldo, prod.Saldo);
+            Assert.Equal(_custo, prod.Custo); 
 
         }
     }
